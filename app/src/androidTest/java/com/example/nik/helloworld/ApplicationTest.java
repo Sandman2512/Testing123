ccce81329170ebe140e64e106f25f27be60247d9
icziemba <icziemba@gmail.com>
2015-03-18 21:38:12 -0500

    Added a test comment
    who gives a shit

--- app/src/androidTest/java/com/example/nik/helloworld/ApplicationTest.java
+++ app/src/androidTest/java/com/example/nik/helloworld/ApplicationTest.java
@@ -3,6 +3,10 @@ package com.example.nik.helloworld;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+/*
+Another comment (yippie)
+*/
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
